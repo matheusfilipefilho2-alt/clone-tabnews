80a2b759adf2160aef01994d3d9dca1ba9905e5a
matheusF <matheus.filipe.filho2@gmail.com>
2025-11-27 15:55:08 -0300

    adds `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,43 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js"
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+
+    dbClient: dbClient,
+    // dryRun : faz um falso teste
+    dryRun: false,
+    // dir: escolhe o diretorio
+    dir: join("infra", "migrations"), // dir = diretorio
+    // direction: escolhe a função da migration
+    direction: "up",
+    verbose: true,
+    // migrationsTable: tabela que armazena as migrations executadas
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    // migrationRunner: executa todas as migrations
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    // migrationRunner: executa todas as migrations
+    const migratedMigraions = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigraions.length > 0) {
+      return response.status(201).json(migratedMigraions);
+    }
+
+    return response.status(200).json(migratedMigraions);
+  }
+  return response.status(405).end();
+}
