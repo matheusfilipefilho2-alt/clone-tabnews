ea1810f54799f5186d73deb52a773871e2c9148f
matheusF <matheus.filipe.filho2@gmail.com>
2025-12-02 13:49:35 -0300

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,42 +3,57 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    // dryRun : faz um falso teste
-    dryRun: true,
-    // dir: escolhe o diretorio
-    dir: join("infra", "migrations"), // dir = diretorio
-    // direction: escolhe a função da migration
-    direction: "up",
-    verbose: true,
-    // migrationsTable: tabela que armazena as migrations executadas
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    // migrationRunner: executa todas as migrations
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
   }
 
-  if (request.method === "POST") {
-    // migrationRunner: executa todas as migrations
-    const migratedMigraions = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
 
-    await dbClient.end();
+  try {
+    dbClient = await database.getNewClient();
 
-    if (migratedMigraions.length > 0) {
-      return response.status(201).json(migratedMigraions);
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      // dryRun : faz um falso teste
+      dryRun: true,
+      // dir: escolhe o diretorio
+      dir: join("infra", "migrations"), // dir = diretorio
+      // direction: escolhe a função da migration
+      direction: "up",
+      verbose: true,
+      // migrationsTable: tabela que armazena as migrations executadas
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      // migrationRunner: executa todas as migrations
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(migratedMigraions);
+    if (request.method === "POST") {
+      // migrationRunner: executa todas as migrations
+      const migratedMigraions = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      await dbClient.end();
+
+      if (migratedMigraions.length > 0) {
+        return response.status(201).json(migratedMigraions);
+      }
+
+      return response.status(200).json(migratedMigraions);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  return response.status(405).end();
 }
