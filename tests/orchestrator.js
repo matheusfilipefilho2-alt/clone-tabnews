1ba5296580e3a94857aee6fec028ecfa31d6213c
matheusF <matheus.filipe.filho2@gmail.com>
2026-02-11 17:46:35 -0300

    fix: remove unused import for Username in orchestrator.js

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
-import Username from "pages/api/v1/users/[username]";
+
 
 async function waitForAllServices() {
   await waitForWebServer();
