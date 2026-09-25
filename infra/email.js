d1d54abc6c861287751decc69acf02d15c9604fb
matheusF <matheus.filipe.filho2@gmail.com>
2026-09-25 15:13:15 -0300

    refactor: improve error login in `email.send()`

--- infra/email.js
+++ infra/email.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { ServiceError } from "./errors";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -11,7 +12,16 @@ const transporter = nodemailer.createTransport({
 });
 
 async function send(mailOptions) {
-  await transporter.sendMail(mailOptions);
+  try {
+   await transporter.sendMail(mailOptions); 
+  } catch(error) {
+    throw new ServiceError({
+      message: "não foi possivel enviar o email",
+      action: "verifique se o serviço de email está disponível.",
+      cause: error,
+      context: mailOptions
+    })
+  }
 }
 
 const email = {
